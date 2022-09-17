00c2086368a26121de8c31bd55bbca70cfdf3b9e
mohammad asghari <mohammadsgr8@gmail.com>
2022-09-17 10:53:53 -0700

    [UPDATE] : add pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Header from '../components/header';
+import Transition from '../components/transition';
+import '../styles/globals.css';
+import { App, Content, ImageProfile, Main } from './../styles/app-style';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <App>
+      <Header/>
+      <Main>
+        <ImageProfile>
+          <div className="glitch">
+            <div className="glitch__item"></div>
+            <div className="glitch__item"></div>
+            <div className="glitch__item"></div>
+            <div className="glitch__item"></div>
+            <div className="glitch__item"></div>
+            {/* <h1 className="glitch__title">YAKUZA</h1> */}
+          </div>
+        </ImageProfile>
+        <Content>
+          <Transition>
+            <Component {...pageProps} />
+          </Transition>
+        </Content>
+      </Main>
+    </App>
+  )
 }
 
 export default MyApp
